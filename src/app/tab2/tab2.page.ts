e953984b15a5e5a170468f3088db4fa5be2a1d3d
Benjamin Silvestre <benjaminsilvestre3103@gmail.com>
2023-08-24 21:39:48 -0300

    Commit inicial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';            // <-- required
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, CommonModule]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {}
+
+  addPhotoToGallery(){
+    this.photoService.addNewToGallery();
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { enableProdMode, importProvidersFrom} from '@angular/core';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -18,3 +19,5 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes),
   ],
 });
+
+defineCustomElements(window);
